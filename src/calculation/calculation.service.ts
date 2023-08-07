a6806d3985e6c47f8b123034618c7c81026b3ffb
Nicole <niipimenta@hotmail.com>
2023-08-06 21:02:43 -0300

    feat:ajustando service

--- src/calculation/calculation.service.ts
+++ src/calculation/calculation.service.ts
@@ -2,27 +2,28 @@ import { Model } from 'mongoose';
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { CreateDto } from './dto/Create.dto';
-import { Calculation } from './entities/calculation.entity';
+import {
+  Calculation,
+  CalculationDocument,
+} from './entities/calculation.entity';
 
 @Injectable()
 export class CalculationService {
   constructor(
-    @InjectModel(Calculation.name) private calculationModel: Model<Calculation>,
+    @InjectModel(Calculation.name)
+    private calculationModel: Model<CalculationDocument>,
   ) {}
 
-  async create(createDto: CreateDto): Promise<Calculation> {
-    const createdCat = new this.calculationModel(createDto);
-    return createdCat.save();
+  create(createDto: CreateDto): Promise<Calculation> {
+    const calculation = new this.calculationModel(createDto);
+    return calculation.save();
   }
-  // private database = [];
-  // async save(calculation) {
-  //   this.database.push(calculation);
-  // }
-  // async read() {
-  //   return this.database;
-  // }
-  // async readById(id: string) {
-  //   const findItem = this.database.find((item) => item.id === id);
-  //   return findItem !== undefined;
-  // }
+
+  findAll() {
+    return this.calculationModel.find();
+  }
+
+  findOne(id: string) {
+    return this.calculationModel.findById(id);
+  }
 }
